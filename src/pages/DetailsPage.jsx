61b37a4de88db6236cab9c5b459408cfe1421c23
MAGNET-CODE <sozan1382dastan@gmail.com>
2024-03-20 21:23:58 +0330

    Changing state management from context to redux-toolkit-in-detailsPage

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -1,34 +1,44 @@
+import { useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { SiOpenproject } from "react-icons/si";
 import { IoMdPricetag } from "react-icons/io";
 import { FaArrowLeft } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProducts } from "../features/product/productSlice";
 
-import { useProductDetails } from "../context/ProduckContext";
 import Loader from "../components/Loader";
 
 import styles from "./DetailsPage.module.css";
 
 function DetailsPage() {
   const { id } = useParams();
+  const dispatch = useDispatch();
 
-  const productDeatils = useProductDetails(+id);
-  const {title, image, price, description, category} = productDeatils;
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [])
+
+  const productDeatils = useSelector((store) =>
+     store.product.products.find((i) => i.id === +id)
+  );
+
+  // const {title, image, price, description, category} = productDeatils;
   if(!productDeatils) return <Loader />;
 
   return (
     <div className={styles.container}>
-      <img src={image} alt={title} />
+      <img src={productDeatils.image} alt={productDeatils.title} />
       <div className={styles.information}>
-        <h3 className={styles.title}>{title}</h3>
-        <p className={styles.description}>{description}</p>
+        <h3 className={styles.title}>{productDeatils.title}</h3>
+        <p className={styles.description}>{productDeatils.description}</p>
         <p className={styles.category}>
           <SiOpenproject />
-          {category}
+          {productDeatils.category}
         </p>
         <div>
           <span className={styles.price}>
             <IoMdPricetag />
-            {price} $
+            {productDeatils.price} $
           </span>
           <Link to="/" >
             <FaArrowLeft />
